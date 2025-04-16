d0a26bc8dfeb7634e68c314b8a2608018df39a43
moazzzmedhat <m.gafer2543@su.edu.eg>
2025-04-16 16:46:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ document.addEventListener('DOMContentLoaded', function() {
         navLinks.classList.toggle('active');
     });
 
+    // Close menu when clicking outside
     document.addEventListener('click', function(event) {
         if (!event.target.closest('.header .links')) {
             navLinks.classList.remove('active');
         }
     });
 
+    // Close menu when clicking on a link
     const navItems = document.querySelectorAll('.header .links .menu li a');
     navItems.forEach(item => {
         item.addEventListener('click', function() {
